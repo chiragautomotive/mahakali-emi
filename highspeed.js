5265d9a153e653bab6de22c125d86567e2e4cf47
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2022-12-07 18:36:44 +0530

    Add files via upload
    initial upload

---
+++ highspeed.js
@@ -0,0 +1,53 @@
+function calculateEMI(){
+  var DP = parseInt(document.getElementById("downpayment").value)
+  var vehiclePrice= 253920
+  var tenureM = parseInt(document.getElementById("tenure").value)
+
+
+  
+  var docuCharges = 8000
+  var loanAmt = vehiclePrice+docuCharges-DP
+  var interestRtPM = 0
+
+  if(loanAmt>=190000){
+    interestRtPM = 0.01138
+  } else {
+    interestRtPM = 0.0094
+  }
+
+  console.log(interestRtPM)
+
+  interestPM = loanAmt*interestRtPM
+
+  var loint = (interestPM*tenureM)+loanAmt
+
+  var installments = Math.round(((interestPM*tenureM)+loanAmt)/tenureM)
+
+  var totVehPrice = Math.round(loint+DP)
+
+  var totInterest = Math.round(interestPM*tenureM)
+
+  document.getElementById("installments").innerText= installments
+  document.getElementById("price").innerText= totVehPrice
+  document.getElementById("interest").innerText= totInterest
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+  
+
+
+}
