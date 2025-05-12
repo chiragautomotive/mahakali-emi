6ccc787434f8972da4b467132f59063672de2e7a
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2025-05-12 11:35:25 +0530

    Update highspeed.js
    PRICE UPDATE

--- highspeed.js
+++ highspeed.js
@@ -1,6 +1,6 @@
 function calculateEMI(){
   var DP = parseInt(document.getElementById("downpayment").value)
-  var vehiclePrice= 279700
+  var vehiclePrice= 272375
   var tenureM = parseInt(document.getElementById("tenure").value)
 
 
